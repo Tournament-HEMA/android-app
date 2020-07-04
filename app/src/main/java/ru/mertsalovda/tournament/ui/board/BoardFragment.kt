8df1f62baade873e0ed2953c55774c19552cb6b5
Korran <korranhorn009@gmail.com>
2020-07-05 00:41:02 +0300

    Add BoardViewModel

--- app/src/main/java/ru/mertsalovda/tournament/ui/board/BoardFragment.kt
+++ app/src/main/java/ru/mertsalovda/tournament/ui/board/BoardFragment.kt
@@ -1,15 +1,16 @@
 package ru.mertsalovda.tournament.ui.board
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.arbelkilani.clock.enumeration.TimeCounterState
 import com.arbelkilani.clock.listener.TimeCounterListener
-import ru.mertsalovda.tournament.data.model.Rule
 import ru.mertsalovda.tournament.databinding.FrBoardBinding
 import java.util.concurrent.TimeUnit
 
@@ -23,7 +24,7 @@ class BoardFragment : Fragment() {
     private var param2: String? = null
 
     private lateinit var mBinding: FrBoardBinding
-    private val mRule = Rule()
+    private lateinit var mViewModel: BoardViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,6 +32,11 @@ class BoardFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mViewModel = ViewModelProvider(requireActivity()).get(BoardViewModel::class.java)
     }
 
     override fun onCreateView(
@@ -45,7 +51,6 @@ class BoardFragment : Fragment() {
             runTimeCounter(time)
             pauseTimeCounter()
         }
-
         mBinding.timer.setOnClickListener {
             when (mBinding.timer.timeCounterState) {
                 TimeCounterState.stopped -> mBinding.timer.runTimeCounter(time)
@@ -54,6 +59,13 @@ class BoardFragment : Fragment() {
             }
         }
 
+        addListeners()
+        observeViewModel()
+
+        return mBinding.root
+    }
+
+    private fun addListeners() {
         mBinding.timer.setTimeCounterListener(object : TimeCounterListener {
             override fun onTimeCounterCompleted() {
                 Toast.makeText(context, "END", Toast.LENGTH_LONG).show()
@@ -62,34 +74,32 @@ class BoardFragment : Fragment() {
             override fun onTimeCounterStateChanged(timeCounterState: TimeCounterState) {
                 Toast.makeText(context, timeCounterState.name, Toast.LENGTH_LONG).show()
             }
-
         })
 
-        mBinding.btnBluePlusOne.setOnClickListener { plusPoints(mBinding.tvBlueCount, 1) }
-        mBinding.btnBluePlusTwo.setOnClickListener { plusPoints(mBinding.tvBlueCount, 2) }
-        mBinding.btnBlueMinusOne.setOnClickListener { minusPoints(mBinding.tvBlueCount, 1) }
-        mBinding.btnBlueMinusTwo.setOnClickListener { minusPoints(mBinding.tvBlueCount, 2) }
+        mBinding.btnBluePlusOne.setOnClickListener { mViewModel.plusBluePoints(1) }
+        mBinding.btnBluePlusTwo.setOnClickListener { mViewModel.plusBluePoints(2) }
+        mBinding.btnBlueMinusOne.setOnClickListener { mViewModel.plusBluePoints(-1) }
+        mBinding.btnBlueMinusTwo.setOnClickListener { mViewModel.plusBluePoints(-2) }
 
-        mBinding.btnRedPlusOne.setOnClickListener { plusPoints(mBinding.tvRedCount, 1) }
-        mBinding.btnRedPlusTwo.setOnClickListener { plusPoints(mBinding.tvRedCount, 2) }
-        mBinding.btnRedMinusOne.setOnClickListener { minusPoints(mBinding.tvRedCount, 1) }
-        mBinding.btnRedMinusTwo.setOnClickListener { minusPoints(mBinding.tvRedCount, 2) }
-
-        return mBinding.root
+        mBinding.btnRedPlusOne.setOnClickListener { mViewModel.plusRedPoints(1) }
+        mBinding.btnRedPlusTwo.setOnClickListener { mViewModel.plusRedPoints(2) }
+        mBinding.btnRedMinusOne.setOnClickListener { mViewModel.plusRedPoints(-1) }
+        mBinding.btnRedMinusTwo.setOnClickListener { mViewModel.plusRedPoints(-2) }
     }
 
-    private fun plusPoints(textView: TextView, points: Int) {
-        val currentPoints = textView.text.toString().toInt()
-        textView.text = (currentPoints + points).toString()
-    }
+    private fun observeViewModel() {
+        mViewModel.redPoints.observe(viewLifecycleOwner, Observer {
+            mBinding.tvRedCount.text = it.toString()
+        })
+        mViewModel.bluePoints.observe(viewLifecycleOwner, Observer {
+            mBinding.tvBlueCount.text = it.toString()
+        })
 
-    private fun minusPoints(textView: TextView, points: Int) {
-        val currentPoints = textView.text.toString().toInt()
-        if (currentPoints - points < 0) {
-            textView.text = "0"
-        } else {
-            textView.text = (currentPoints - points).toString()
-        }
+        mViewModel.winner.observe(viewLifecycleOwner, Observer {
+            if (it.isNotEmpty()) {
+                Toast.makeText(context, it, Toast.LENGTH_LONG).show()
+            }
+        })
     }
 
     companion object {
--- app/src/main/java/ru/mertsalovda/tournament/ui/board/BoardViewModel.kt
+++ app/src/main/java/ru/mertsalovda/tournament/ui/board/BoardViewModel.kt
@@ -1,6 +1,44 @@
 package ru.mertsalovda.tournament.ui.board
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import ru.mertsalovda.tournament.data.model.Rule
+import java.util.concurrent.TimeUnit
 
 class BoardViewModel : ViewModel() {
+
+    private val mRule = Rule("Правила", 10, 4, TimeUnit.MINUTES.toMillis(2))
+
+    private val _redPoints = MutableLiveData(0)
+    val redPoints: MutableLiveData<Int> = _redPoints
+    private val _bluePoints = MutableLiveData(0)
+    val bluePoints: MutableLiveData<Int> = _bluePoints
+    private val _winner = MutableLiveData("")
+    val winner: MutableLiveData<String> = _winner
+
+    fun plusRedPoints(points: Int) {
+        val currentPoints = redPoints.value ?: 0
+        if (currentPoints + points < 0) {
+            redPoints.postValue(0)
+        } else {
+            redPoints.postValue(currentPoints + points)
+        }
+
+        if (currentPoints + points >= mRule.counterWin && currentPoints + points > bluePoints.value!!){
+            winner.postValue("Красный победил")
+        }
+    }
+
+    fun plusBluePoints(points: Int) {
+        val currentPoints = bluePoints.value ?: 0
+        if (currentPoints + points < 0) {
+            bluePoints.postValue(0)
+        } else {
+            bluePoints.postValue(currentPoints + points)
+        }
+
+        if (currentPoints + points >= mRule.counterWin && currentPoints + points > redPoints.value!!){
+            winner.postValue("Синий победил")
+        }
+    }
 }
